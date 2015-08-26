3f15f35fee7ccf834bd77f35e08693631b8e88a2
Aleksey Khoroshev <aleksxor@gmail.com>
2015-08-26 17:04:06 +0700

    add timeoutSuccess, timeoutFailure props [#101389330]

--- app/scripts/react/components/SmartSelect/index.jsx
+++ app/scripts/react/components/SmartSelect/index.jsx
@@ -17,11 +17,17 @@ export default class SmartSelect extends Component {
         color_rgb: PropTypes.string,
       })
     ).isRequired,
+    timeoutSuccess: PropTypes.number,
+    timeoutFailure: PropTypes.number,
     updateUrl: PropTypes.string,
     value: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.number,
     ]).isRequired,
+  }
+  static defaultProps = {
+    timeoutSuccess: 3,
+    timeoutFailure: 3,
   }
   state = {
     errorMsg: void 0,
@@ -34,8 +40,15 @@ export default class SmartSelect extends Component {
     this.setState({ value: this.props.value });
   }
 
+  resetRequestState(timeout) {
+    this.errTimeoutId = window.setTimeout(() => this.setState({
+      errorMsg: null,
+      requestStatus: null,
+    }), timeout * 1000);
+  }
+
   onChange(value) {
-    const { fieldName, method, updateUrl } = this.props;
+    const { fieldName, method, timeoutSuccess, timeoutFailure, updateUrl } = this.props;
     const oldValue = this.state.value;
 
     if (this.errTimeoutId) {
@@ -52,13 +65,14 @@ export default class SmartSelect extends Component {
       url: updateUrl,
       data: { [fieldName]: value },
     })
-      .done(() =>
+      .done(() => {
         this.setState({
           value,
           errorMsg: null,
           requestStatus: constants.REQUEST_OK,
-        })
-      )
+        });
+        this.resetRequestState(timeoutSuccess);
+      })
       .fail((jq) => {
         this.setState({
           value: oldValue,
@@ -66,13 +80,8 @@ export default class SmartSelect extends Component {
           requestStatus: constants.REQUEST_ERROR,
         });
         window.alert(jq.responseText);
-      })
-      .always(() =>
-        this.errTimeoutId = window.setTimeout(() => this.setState({
-          errorMsg: null,
-          requestStatus: null,
-        }), 10 * 1000)
-      );
+        this.resetRequestState(timeoutFailure);
+      });
   }
   
   render() {
